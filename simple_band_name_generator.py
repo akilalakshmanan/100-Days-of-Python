8407255503be1bbb92b3281ec3bb6ce7ddfb983e
akilalakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2021-07-29 21:18:00 +0530

    Delete simple_band_name_generator.py

--- simple_band_name_generator.py
+++
@@ -1,9 +0,0 @@
-# Day 1 Project Band Name Generator
-
-print("Welcome to Project Band Name Generator\n")
-
-city_name = input("What is the name of the city you grew up?\n")
-
-pet_name = input("What is your pet name\n")
-
-print("Your band name is " + city_name + " " + pet_name)
