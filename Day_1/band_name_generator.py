c06a023e8ad8312b727e67d28e86f9b9dd7ea833
akilalakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2021-08-02 13:44:09 +0530

    Concatenation of strings in python

--- Day_1/band_name_generator.py
+++ Day_1/band_name_generator.py
@@ -4,6 +4,6 @@ print("Welcome to Project Band Name Generator\n")
 
 city_name = input("What is the name of the city you grew up?\n")
 
-pet_name = input("What is your pet name\n")
+pet_name = input("What is your pet name\n") 
 
 print("Your band name is " + city_name + " " + pet_name)
