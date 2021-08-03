d35bf4636619820c52dcd955c5c0c92282894d27
akilalakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2021-08-03 19:28:39 +0530

    Escaping the Maze game

--- Day_6/escaping_maze.py
+++ Day_6/escaping_maze.py
@@ -7,7 +7,7 @@ while not at_goal():
     if right_is_clear():
         turn_right()
         move()
-    elif front_is_clear():
+    elif front_is_clear(): 
         move()
     else:
         turn_left()
