b6ab6990fc9c6502a08b71dada653c080849a113
akilalakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2021-08-08 00:41:55 +0530

    Beginner - Blackjack Game - Capstone Project!

--- Day_11/blackjack_game.py
+++ Day_11/blackjack_game.py
@@ -8,7 +8,7 @@ def deal_card():
     """Returns a random card from the deck."""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
-    return card
+    return card 
 
 
 def calculate_score(cards):
