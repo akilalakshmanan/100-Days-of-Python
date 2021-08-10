fa6b4a6b5330d73a93c8c90ad663756fbe2193f1
akilalakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2021-08-10 12:01:18 +0530

    Update guess_number.py

--- Day_12/guess_number.py
+++ Day_12/guess_number.py
@@ -1,34 +1,68 @@
-# This is a guess the number game.
+# Guess the number game
 import random
-guessesTaken = 0
- 
-print('Hello! What is your name?')
-myName = input()
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print("Welcome to number guessing game!")
+print("I'm thinking of a number between 1 and 100.")
+attempts_rem_easy = 10
+attempts_rem_hard = 5
+difficulty_level = input(
+    "Choose a difficulty level. Type 'easy' or 'hard': ").lower()
 
 
-while guessesTaken < 6:
-    print('Take a guess.')  # There are four spaces in front of print.
-    guess = input()
-    guess = int(guess)
-    guessesTaken = guessesTaken + 1
+def game_easy():
+    number = random.randint(1, 100)
+    print(f"the guessed number is: {number}")
+    global attempts_rem_easy
+    game_over = False
+    print(f"You have {attempts_rem_easy} chances to guess the number")
+    while(attempts_rem_easy != 0) and (not game_over):
+        user_guess = int(input("Make a guess: "))
+        if user_guess > number:
+            print("Your guess is too high!")
+            attempts_rem_easy = attempts_rem_easy - 1
+            print(f"You have {attempts_rem_easy} chances to guess the number")
+        elif user_guess < number:
+            print("Your guess is too low!")
+            attempts_rem_easy = attempts_rem_easy - 1
+            print(f"You have {attempts_rem_easy} chances to guess the number")
+        elif user_guess == number:
+            print(
+                f"Good job! You have guessed the {number} with {attempts_rem_easy} guesses remaining")
+            game_over = True
 
-    if guess < number:
-        # There are eight spaces in front of print.
-        print('Your guess is too low.')
+    if user_guess != number and attempts_rem_easy == 0:
+        print(f"Wrong guess.. You have used all your attempts to guess..")
+        print(f"The actual number is {number}.")
+        game_over = True
 
-    if guess > number:
-        print('Your guess is too high.')
 
-    if guess == number:
-        break
+def game_hard():
+    number = int(random.randint(1, 100))
+    print(f"the guessed number is: {number}")
+    global attempts_rem_hard
+    game_over = False
+    print(f"You have {attempts_rem_hard} chances to guess the number")
+    while(attempts_rem_hard != 0) and (not game_over):
+        user_guess = int(input("Make a guess: "))
+        if user_guess > number:
+            print("Your guess is too high!")
+            attempts_rem_hard = attempts_rem_hard - 1
+            print(f"You have {attempts_rem_hard} chances to guess the number")
+        elif user_guess < number:
+            print("Your guess is too low!")
+            attempts_rem_hard = attempts_rem_hard - 1
+            print(f"You have {attempts_rem_hard} chances to guess the number")
+        elif user_guess == number:
+            print(
+                f"Good job! You have guessed the {number} with {attempts_rem_hard} guesses remaining")
+            game_over = True
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' +
-          guessesTaken + ' guesses!')
+    if user_guess != number and attempts_rem_hard == 0:
+        print(f"Wrong guess.. You have used all your attempts to guess..")
+        print(f"The actual number is {number}.")
+        game_over = True
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+
+if difficulty_level == 'easy':
+    game_easy()
+else:
+    game_hard()
